98a00b9881f2cac47d859806538a29eea5fdb5e0
ogjex <m.agnu.ha@gmail.com>
2023-11-12 20:56:59 +0100

    add most tapdance

--- keymap.c
+++ keymap.c
@@ -28,42 +28,52 @@ typedef struct {
 enum {
     TD_RESET,
     TD_DELETE,
+    TD_BSPACE,
     TD_AE_ENTER,
     TD_TEST_STRING,
+    TD_AA_DK,
+    TD_OE_DK,
+    TD_APP_TAB,
+    TD_LSKIP,
+    TD_RSKIP,
+    TD_HOME_P,
+    TD_END_N,
+    TD_ESC_TM,
+    TD_NEXT_T,
+    TD_PREV_T
 };
 
 // define the various layers
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
     // Base key input layer--------------------         -----------------------------------------------
-    TD(TD_TEST_STRING), KC_W, KC_E, KC_R, KC_T,         KC_Y, KC_U, KC_I, KC_O, KC_P,
+    KC_Q, KC_W, KC_E, KC_R, KC_T,                       KC_Y, KC_U, KC_I, TD(TD_OE_DK), KC_P,
     //-----------------------------------------         -----------------------------------------------
-    KC_A, KC_S, KC_D, KC_F, KC_G,                       KC_H, KC_J, KC_K, KC_L, TD(TD_AE_ENTER),
+    TD(TD_AA_DK), KC_S, KC_D, KC_F, KC_G,               KC_H, KC_J, KC_K, KC_L, TD(TD_AE_ENTER),
     //-----------------------------------------         -----------------------------------------------
-    KC_Z, KC_X , KC_C, KC_V, TD(TD_DELETE),           KC_B, KC_N, KC_M, KC_COMM, KC_DOT,
+    KC_Z, KC_X , KC_C, KC_V, TD(TD_DELETE),             KC_B, KC_N, KC_M, KC_COMM, KC_DOT,
     //-----------------------------------------         -----------------------------------------------
                         OSM(MOD_LSFT), KC_SPC,          OSL(1), OSM(MOD_LCTL)
     ),
 
     [1] = LAYOUT(
     // Signs and symbols layer, from layer 0---         ----------------------------------------------
-    KC_ESC, KC_AT, KC_HASH, KC_DLR, KC_PERC,            KC_AMPR, KC_PSLS, KC_PIPE, KC_PMNS, KC_QUES,
+    TD(TD_ESC), KC_AT, KC_HASH, KC_DLR, KC_PERC,        KC_AMPR, KC_PSLS, KC_PIPE, KC_PMNS, KC_QUES,
     // ----------------------------------------         ----------------------------------------------
-    KC_TAB, KC_CIRC, KC_AT, KC_QUOT, KC_PIPE,            KC_EXLM, KC_ASTR, KC_LPRN, KC_QUES, KC_ENT,
+    TD(TD_APP_TAB), KC_CIRC, KC_AT, KC_QUOT, KC_PIPE,   KC_EXLM, KC_ASTR, KC_LPRN, KC_QUES, KC_ENT,
     // ----------------------------------------         ----------------------------------------------
     KC_LT, KC_GT, KC_TILD, KC_GRV, KC_TRNS,             KC_LBRC, KC_LCBR, KC_RCBR, KC_RBRC, TO(3),
     // ----------------------------------------         ----------------------------------------------
                             TO(0), KC_TRNS,             TO(2), KC_TRNS
     ),
 
-
     [2] = LAYOUT(
     // Numpad layer, from layer 1--------------         ---------------------------------------------
-    KC_ESC, KC_TRNS, KC_PSLS, KC_PAST, KC_PMNS,         KC_PEQL, KC_7, KC_8, KC_9, KC_BSPC,
+    KC_TRNS, KC_TRNS, KC_PSLS, KC_PAST, KC_PMNS,         KC_PEQL, KC_7, KC_8, KC_9, TD(TD_),
     // ----------------------------------------         ---------------------------------------------
-    KC_TAB, KC_NO, KC_NO, KC_NO, KC_PPLS,               KC_PERC, KC_4, KC_5, KC_6, KC_ENT,
+    KC_TRNS, KC_NO, KC_NO, KC_NO, KC_PPLS,               KC_PERC, KC_4, KC_5, KC_6, KC_ENT,
     // ----------------------------------------         ---------------------------------------------
-    KC_LSFT, KC_LCTL, KC_LGUI, KC_LALT, TD(TD_DELETE),         KC_0, KC_1, KC_2, KC_3, KC_PEQL,
+    KC_LSFT, KC_LCTL, KC_LGUI, KC_LALT, KC_TRNS,         KC_0, KC_1, KC_2, KC_3, KC_PEQL,
     // ----------------------------------------         ---------------------------------------------
                                 TO(0), KC_TRNS,         TO(4), KC_TRNS
     ),
@@ -71,18 +81,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [3] = LAYOUT(
     // Navigation layer, from base layer 0-----         --------------------------------------------
-    KC_ESC, KC_WH_L, KC_MS_U, KC_WH_R, KC_WH_U,         KC_NO, KC_NO, KC_NO, KC_NO, KC_BSPC,
+    KC_TRNS, KC_WH_L, KC_MS_U, KC_WH_R, KC_WH_U,         KC_NO, KC_NO, KC_NO, KC_NO, KC_BSPC,
     // ----------------------------------------         --------------------------------------------
-    KC_TAB, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D,         KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_ENT,
+    KC_TAB, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D,         TD(TD_LSKIP), KC_DOWN, KC_UP, TD(TD_RSKIP), KC_ENT,
     // ----------------------------------------         --------------------------------------------
-    KC_BTN2, KC_CUT, KC_COPY, KC_PASTE, KC_DEL,           KC_ACL0, KC_ACL1, KC_ACL2, KC_PGDN, KC_PGUP,
+    KC_BTN2, KC_CUT, KC_COPY, KC_PASTE, KC_DEL,         KC_ACL0, KC_ACL1, KC_ACL2, KC_PGDN, KC_PGUP,
     // ----------------------------------------         --------------------------------------------
-                            TO(0), OSM(MOD_LSFT),         KC_BTN1, OSM(MOD_LCTL)
+                            TO(0), OSM(MOD_LSFT),       KC_BTN1, OSM(MOD_LCTL)
     ),
 
     [4] = LAYOUT(
     // Reset layer, from layer 3---------------         --------------------------------------------
-    TD(TD_RESET), KC_Q, KC_NO, KC_NO, KC_NO,           KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    TD(TD_RESET), KC_Q, KC_NO, KC_NO, KC_NO,            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
     // ----------------------------------------         ---------------------------------------------
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
     // ----------------------------------------         ---------------------------------------------
@@ -205,6 +215,38 @@ void td_ae_enter(tap_dance_state_t *state, void *user_data) {
     }
 }
 
+// defining left arrow tapdance
+void td_left_skip(tap_dance_state_t *state, void *user_data) {
+    tap_state.state = dance_state(state);
+    switch (tap_state.state) {
+        case TD_SINGLE_TAP:
+            tap_code(KC_LEFT);
+            break;
+        case TD_SINGLE_HOLD:
+            SEND_STRING(SS_DOWN(X_LCTL) SS_TAP(X_LEFT) SS_UP(X_LCTL));
+            break;
+        default:
+            reset_tap_dance(state);
+            break;
+    }
+}
+
+// defining right arrow tapdance
+void td_right_skip(tap_dance_state_t *state, void *user_data) {
+    tap_state.state = dance_state(state);
+    switch (tap_state.state) {
+        case TD_SINGLE_TAP:
+            tap_code(KC_RIGHT);
+            break;
+        case TD_SINGLE_HOLD:
+            SEND_STRING(SS_DOWN(X_LCTL) SS_TAP(X_RIGHT) SS_UP(X_LCTL));
+            break;
+        default:
+            reset_tap_dance(state);
+            break;
+    }
+}
+
 // defining delete key macro
 void td_delete(tap_dance_state_t *state, void *user_data) {
     tap_state.state = dance_state(state);
@@ -260,14 +302,17 @@ void safe_reset(tap_dance_state_t *state, void *user_data) {
     }
 }
 
-// defining tab switcher tapdance key
-void td_tabswitch(tap_dance_state_t *state, void *user_data) {
+// defining app and tab switcher tapdance key
+void td_apptab_switch(tap_dance_state_t *state, void *user_data) {
     tap_state.state = dance_state(state);
     switch (tap_state.state) {
         case TD_SINGLE_TAP:
-            SEND_STRING(SS_DOWN(X_LALT) SS_TAP(X_TAB) SS_UP(X_LALT));
+            tap_code(KC_TAB);
             break;
         case TD_SINGLE_HOLD:
+            SEND_STRING(SS_DOWN(X_LALT) SS_TAP(X_TAB) SS_UP(X_LALT));
+            break;
+        case TD_DOUBLE_HOLD:
             SEND_STRING(SS_DOWN(X_LGUI) SS_TAP(X_TAB) SS_UP(X_LGUI));
 
             break;
@@ -276,16 +321,39 @@ void td_tabswitch(tap_dance_state_t *state, void *user_data) {
     }
 }
 
-// defining home tapdance key
-void td_home(tap_dance_state_t *state, void *user_data) {
+// defining esc and task manager tapdance key
+void td_esc_tm(tap_dance_state_t *state, void *user_data) {
     tap_state.state = dance_state(state);
     switch (tap_state.state) {
         case TD_SINGLE_TAP:
-            tap_code(KC_HOME);
-
+            tap_code(KC_ESCAPE);
             break;
-        case TD_SINGLE_HOLD:
-            SEND_STRING(SS_DOWN(X_LCTL) SS_TAP(X_BSPC) SS_UP(X_LCTL));
+        case TD_DOUBLE_HOLD:
+            SEND_STRING(SS_TAP(X_LCTL) SS_TAP(X_LALT) SS_TAP(X_DELT));
+            break;
+        default:
+            break;
+    }
+}
+
+// defining previous tab tapdance key
+void td_next_tab(tap_dance_state_t *state, void *user_data) {
+    tap_state.state = dance_state(state);
+    switch (tap_state.state) {
+        case TD_SINGLE_TAP:
+            SEND_STRING(SS_DOWN(X_LCTL) SS_DOWN(X_LSFT) SS_TAP(X_TAB) SS_UP(X_LCTL) SS_UP(X_LSFT));
+            break;
+        default:
+            break;
+    }
+}
+
+// defining next tab tapdance key
+void td_next_tab(tap_dance_state_t *state, void *user_data) {
+    tap_state.state = dance_state(state);
+    switch (tap_state.state) {
+        case TD_SINGLE_TAP:
+            SEND_STRING(SS_DOWN(X_LCTL) SS_TAP(X_TAB) SS_UP(X_LCTL));
 
             break;
         default:
@@ -293,14 +361,53 @@ void td_home(tap_dance_state_t *state, void *user_data) {
     }
 }
 
+// defining home and previous desktop tapdance key
+void td_home_prev(tap_dance_state_t *state, void *user_data) {
+    tap_state.state = dance_state(state);
+    switch (tap_state.state) {
+        case TD_SINGLE_TAP:
+            tap_code(KC_HOME);
+            break;
+        case TD_SINGLE_HOLD:
+            SEND_STRING(SS_DOWN(X_LCTL) SS_DOWN(X_LALT) SS_TAP(X_LEFT) SS_UP(X_LALT) SS_UP(X_LCTL));
+            break;
+        default:
+            break;
+    }
+}
+
+// defining end and next desktop tapdance key
+void td_end_next(tap_dance_state_t *state, void *user_data) {
+    tap_state.state = dance_state(state);
+    switch (tap_state.state) {
+        case TD_SINGLE_TAP:
+            tap_code(KC_END);
+            break;
+        case TD_SINGLE_HOLD:
+            SEND_STRING(SS_DOWN(X_LCTL) SS_DOWN(X_LALT) SS_TAP(X_RIGHT) SS_UP(X_LALT) SS_UP(X_LCTL));
+            break;
+        default:
+            break;
+    }
+}
 
 //Associate our tap dance key with its functionality
 tap_dance_action_t tap_dance_actions[] = {
-  [TD_RESET] = ACTION_TAP_DANCE_FN(safe_reset),
-  [TD_DELETE] = ACTION_TAP_DANCE_FN(td_delete),
-  [TD_AE_ENTER] = ACTION_TAP_DANCE_FN(td_ae_enter),
-  [TD_TEST_STRING] = ACTION_TAP_DANCE_FN(td_send_success_strings)
-
+    [TD_RESET] = ACTION_TAP_DANCE_FN(safe_reset),
+    [TD_DELETE] = ACTION_TAP_DANCE_FN(td_delete),
+    [TD_BSPACE] = ACTION_TAP_DANCE_FN(td_bspace),
+    [TD_AE_ENTER] = ACTION_TAP_DANCE_FN(td_ae_enter),
+    [TD_TEST_STRING] = ACTION_TAP_DANCE_FN(td_send_success_strings),
+    [TD_AA_DK] = ACTION_TAP_DANCE_FN(td_aa),
+    [TD_OE_DK] = ACTION_TAP_DANCE_FN(td_oe),
+    [TD_APP_TAB] = ACTION_TAP_DANCE_FN(td_apptab_switch),
+    [TD_LSKIP] = ACTION_TAP_DANCE_FN(td_left_skip),
+    [TD_RSKIP] = ACTION_TAP_DANCE_FN(td_right_skip),
+    [TD_HOME_P] = ACTION_TAP_DANCE_FN(td_home_prev),
+    [TD_END_N] = ACTION_TAP_DANCE_FN(td_end_next),
+    [TD_ESC_TM] = ACTION_TAP_DANCE_FN(td_esc_tm),
+    [TD_NEXT_T] = ACTION_TAP_DANCE_FN(td_next_tab),
+    [TD_PREV_T] = ACTION_TAP_DANCE_FN(td_prev_tab)
 };
 
 // old code from here
