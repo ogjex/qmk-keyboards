cf6ffd3a2901535de1a341123615155131ab7f8c
ogjex <m.agnu.ha@gmail.com>
2023-12-05 17:24:00 +0100

    change nav lshift

--- config.h
+++ config.h
@@ -18,7 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 #undef TAPPING_TERM
-#define TAPPING_TERM 240
+#define TAPPING_TERM 200
 
 #undef MOUSEKEY_DELAY
 #define MOUSEKEY_DELAY 5
